bd784da0d25b70c74529f0c40933c91523efb331
giorgioscappaticcio <ooohh1@hotmail.it>
2014-08-06 15:57:14 +0100

    new gallery added

--- app/src/gallerypopup/directives/gallerypopup.js
+++ app/src/gallerypopup/directives/gallerypopup.js
@@ -7,24 +7,42 @@ angular.module('skinandInkApp')
       restrict: 'AE',
       link: function link($scope, element, attrs, $log) {
       
-      	$scope.quantity = 5;
-      	$scope.windowHeight = (window.innerHeight - 80) + 'px';
-      	$scope.fullHeight = (window.innerHeight) + 'px';
+      	$scope.photos = [];
 
-      	var windowEl = angular.element($window);
-      	var handler = function() {
-      	    $scope.scroll = (windowEl.scrollTop() - 70) + 'px';
-      	}
-      	windowEl.on('scroll', $scope.$apply.bind($scope, handler));
-      	handler();
-      	  
+      	// initial image index
+      	$scope._Index = 0;
+
+      	// if a current image is the same as requested image
+      	$scope.isActive = function (index) {
+      	    return $scope._Index === index;
+      	};
+
+      	// show prev image
+      	$scope.showPrev = function () {
+      	    $scope._Index = ($scope._Index > 0) ? --$scope._Index : $scope.photos.length - 1;
+      	};
+
+      	// show next image
+      	$scope.showNext = function () {
+      	    $scope._Index = ($scope._Index < $scope.photos.length - 1) ? ++$scope._Index : 0;
+      	};
+
+      	// show a certain image
+      	$scope.showPhoto = function (index) {
+      	    $scope._Index = index;
+      	};
+
+
+      	//$scope.windowHeight = (window.innerHeight - 80) + 'px';
+      	$scope.windowHeight = (window.innerHeight - 220) + 'px';
+      	$scope.fullHeight = (window.innerHeight) + 'px';
 
       	// I am the TRUTHY expression to watch.
       	var expression = attrs.gallerypopup;
 
       	var fbAlbumId = null;
 
-      	$scope.fbAlbum = 'studio'
+      	
       	// I check to see the default display of the
 		// element based on the link-time value of the
 		// model we are watching.
@@ -45,15 +63,14 @@ angular.module('skinandInkApp')
 		  			fbAlbumId = 473296213837;
 		  		break;
 		  		default :
-		  			fbAlbumId = 0;
+		  			fbAlbumId = 10152187799888838;
 		  		break;
 
 		  		return fbAlbumId;
 		  	}
 
-		  	if (fbAlbumId == 0) {
+		  	if ($scope.fbAlbum == 'close') {
 		  		// this controls the button to close the gallery
-		  		
 		  		$('.back_home').removeClass('slideInDown').addClass('slideOutUp');
 		  		setTimeout(function(){
 		  			element.removeClass('slideInLeft').addClass('slideOutLeft');
@@ -62,17 +79,23 @@ angular.module('skinandInkApp')
 		  				$('#h').show().removeClass('slideOutLeft').addClass('slideInLeft');
 		  			},600);
 		  		},200);
-		  		
 		  		return;
 		  	} else {
 		  		CommonMain.getFBPhotos(fbAlbumId).then( function(d) {
 		  			// success
-		  			
+		  			$scope.photos = [];
 		  			if(d){
 		  				$scope.photosObjGallery = d.data;
-		  		
+		  				for (var i=0; i<d.data.length; i++){
+		  					var ciao = {
+		  						src: d.data[i].images[0].source, 
+		  						thumb:d.data[i].images[d.data[i].images.length -1].source,
+		  						 desc:'boh'
+		  						}
+		  					$scope.photos.push(ciao);
+		  				}
 		  			}
-		  		}, function(d) {
+				}, function(d) {
 		  			// request rejected (error)
 		  			$scope.photosObjGallery = {};
 		  		});
@@ -86,49 +109,36 @@ angular.module('skinandInkApp')
 			}
 			// Show element.
 			if ( newValue ) {
-				$('#h').removeClass('slideInLeft').addClass('slideOutLeft');
-				imagesLoaded( document.querySelector('.grid-wrap'), function( instance ) {
-				  var allDone = true;
-				  if (allDone){
-				  	setTimeout(function(){
-				  		$('#h').hide();
-				  		element.show().removeClass('slideOutLeft').addClass('slideInLeft');
-				  		new CBPGridGallery( document.getElementById( 'grid-gallery' ) );
-				  		setTimeout(function(){
-				  			$('.back_home').removeClass('slideOutUp').addClass('slideInDown');
-				  		},200);	
-				  	},600);
-				  }
+				$scope._Index = 0;
+				var body = $document.find('body').eq(0);
+				body.animate({scrollTop:0}, '500', 'swing', function() { 
+				   	$('#h').removeClass('slideInLeft').addClass('slideOutLeft');
+			     	setTimeout(function(){
+			     		$('#h').hide();
+			     		element.show().removeClass('slideOutLeft').addClass('slideInLeft');
+			     		setTimeout(function(){
+			     			$('.back_home').removeClass('slideOutUp').addClass('slideInDown');
+			     		},200);	
+			     	},600);
 				});
+
 			// Hide element.
 			} else {
+				
 				if(element.is(":visible")){
 					$('.back_home').removeClass('slideInDown').addClass('slideOutUp');
 					setTimeout(function(){
 						element.removeClass('slideInLeft').addClass('slideOutLeft');
-						imagesLoaded( document.querySelector('.grid-wrap'), function( instance ) {
-							var allDone = true;
-							if (allDone){
-								setTimeout(function(){
-									element.removeClass('slideOutLeft').addClass('slideInLeft');
-									new CBPGridGallery( document.getElementById( 'grid-gallery' ) );
-									setTimeout(function(){
-										$('.back_home').removeClass('slideOutUp').addClass('slideInDown');
-									},200);
-								},600);
-							}
-						});
+						setTimeout(function(){
+							element.removeClass('slideOutLeft').addClass('slideInLeft');
+							setTimeout(function(){
+								$('.back_home').removeClass('slideOutUp').addClass('slideInDown');
+							},200);
+						},600);
 					},200);
-					
-				} else {
-					$('#h').removeClass('slideOutLeft').addClass('slideInLeft');
-					setTimeout(function(){
-						$('#h').hide();
-						element.show().removeClass('slideOutLeft').addClass('slideInLeft');
-					},600);
-				}
+				} 
 			}
 		});
-		}
+	   }
     };
   });
